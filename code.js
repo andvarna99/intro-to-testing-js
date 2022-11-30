61d4881d11f4b75a09c3bdde3c88f7f702e90a5a
Andrea Varnado <andrea.h.varnado@gmail.com>
2022-11-30 15:42:03 -0600

    exercise step 7

--- code.js
+++ code.js
@@ -4,13 +4,7 @@ function helloWorld() {
 }
 
 //sayHello function
-function sayHello(input) {
-    if (input === "Pat") {
-        return "Hello, Pat!"
-    }else if (input === "Alex") {
-            return "Hello, Alex!"
-        } else {
-            return "Hello, Jane!";
-        }
+function sayHello(name) {
+    return "Hello, " + name + "!";
 }
-sayHello("Alex");
+sayHello();
